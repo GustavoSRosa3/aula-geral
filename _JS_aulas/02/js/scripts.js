7fc7e19a9c778bfcff9ccdd46cb893d5a1e58fba
GUSTAVO SORRILLA ROSA <gustavo.srosa3@senacsp.edu.br>
2023-10-09 21:00:18 -0300

    intervalo no //texto

--- _JS_aulas/02/js/scripts.js
+++ _JS_aulas/02/js/scripts.js
@@ -1,3 +1,38 @@
 console.log(2);
 console.log(10); 
 console.log("Meu nome é Lord Darkus");
+console.log(5+1);
+console.log(typeof 2);
+console.log(typeof 10); 
+console.log(typeof "Meu nome é Lord Darkus");
+console.log(typeof 5+1);
+
+// typeof = mostra o tipo do campo no F12
+// comentário de linha única
+
+/** 
+ comentário
+ longo
+ demais
+*/
+
+// Operações matemática
+
+console.log(2+4);
+console.log(10-5);
+console.log(5*4);
+console.log(20/4);
+
+// abre no brownser e aperta F12 e depois em "Console" para ver o resultado das operações
+
+// Texto
+console.log("Eu sou um texto");
+console.log('Tbm sou um texto');
+console.log('Sou um texto mais modero');
+
+console.log(typeof "Eu sou um texto");
+console.log(typeof 'Tbm sou um texto');
+console.log(typeof 'Sou um texto mais modero');
+
+console.log("Testando \n quebra de linha"); // \n = quebra de linha
+console.log("Testando \t espaçamento de tab"); // \t = espaçamento de tab
